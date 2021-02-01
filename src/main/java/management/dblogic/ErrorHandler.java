6265668e9a5e0997585e32d65041e4ab46a4aa61
Paweł Boruta <pawello99@o2.pl>
2021-02-01 20:24:10 +0100

    Update ErrorHandler.java

--- src/main/java/management/dblogic/ErrorHandler.java
+++ src/main/java/management/dblogic/ErrorHandler.java
@@ -13,6 +13,27 @@ public class ErrorHandler {
         else if(errorCode == 1) {
             content = "Nie można dodać ponownie tego samego elementu";
         }
+        else if(errorCode == 2290) {
+            content = "Wprowadzono niepoprawne dane, sprawdź ich zgodność";
+        }
+        else if(errorCode == 1400) {
+            content = "Wypełnij wszystkie wymagane pola";
+        }
+        else if(errorCode == 20004) {
+            content = "Data koncertu, który chcesz dodać do trasy, nie mieści się w ramach czasowych tej trasy";
+        }
+        else if(errorCode == 20003) {
+            content = "Pracownik jest przydzielony do innego koncertu tego dnia";
+        }
+        else if(errorCode == 20001) {
+            content = "Pracownik jest przydzielony do innej sesji tego dnia";
+        }
+        else if(errorCode == 20000) {
+            content = "Artysta ma już zaplanowaną sesję w tym czasie";
+        }
+        else if(errorCode == 20002) {
+            content = "Artysta ma już zaplanowany koncert w tym czasie";
+        }
         else {
             content = "Nieznany błąd. Kod błędu: " +  errorCode;
         }
